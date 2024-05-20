cb4a9455adc860f23133fc11534a6af2157429ea
東谷一輝 <hokushi97@gmail.com>
2024-05-20 18:15:50 +0900

    拡大表示機能追加

--- src/pages/components/Image.tsx
+++ src/pages/components/Image.tsx
@@ -2,21 +2,50 @@
 import { Box } from '@mui/material';
 
 export interface ImageBoxProps {
-    /** 画像のURL */
-    src?: string;
-    alt?: string;
-    width?: string
-    height?: string;
+  /** 画像のURL */
+  src?: string;
+  /** 画面のALT */
+  alt?: string;
+  /** 写真の幅 */
+  width?: string;
+  /** 写真の高さ */
+  height?: string;
+  /** 拡大表示するかどうか */
+  expandImage?: boolean;
 }
 
-const ImageBox = (props:ImageBoxProps) => {
-    const { src, alt, width, height } = props
+const ImageBox = (props: ImageBoxProps) => {
+  const { src, alt, width, height, expandImage } = props;
 
   return (
     <Box>
-       <img src={src} alt={alt} style={{ width: width, height: height }} />
+      {expandImage ? (
+        <a href={src} target="_blank" rel="noopener noreferrer">
+          <img
+            src={src}
+            alt={alt}
+            style={{
+              width: width,
+              height: height,
+              marginLeft: '3px',
+              marginRight: '3px',
+            }}
+          />
+        </a>
+      ) : (
+        <img
+          src={src}
+          alt={alt}
+          style={{
+            width: width,
+            height: height,
+            marginLeft: '3px',
+            marginRight: '3px',
+          }}
+        />
+      )}
     </Box>
   );
 };
 
-export default ImageBox
+export default ImageBox;
